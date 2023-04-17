c0869daaf21f4d6578af0f86480c5cd107080076
Anthony Panarello <aPanarello20@gmail.com>
2023-04-17 15:12:01 -0400

    Added print_uint_color and print_uint functions to print unsigned integers

--- src/shell/print_int.c
+++ src/shell/print_int.c
@@ -3,12 +3,16 @@
 #include <stdint.h>
 
 void print_int_color(int i, unsigned char attribute) {
+    char is_negative = 0;
     if(i < 0) {
         __Scratch_print_character_color('-', attribute);
-        i *= -1;
+        is_negative = 1;
     }
+    print_uint_color(is_negative ? (i * -1) : i, attribute);
+}
 
-    uint8_t buf[20];
+void print_uint_color(unsigned int i, unsigned char attribute) {
+    uint8_t buf[11];
     uint8_t count = 0;
 
     while(i) {
@@ -18,8 +22,8 @@ void print_int_color(int i, unsigned char attribute) {
 
     if(count == 0) __Scratch_print_character_color('0', attribute);
     else {
-        for(i = count - 1; i >= 0; i--) {
-            __Scratch_print_character_color(buf[i], attribute);
+        for(int j = count - 1; j >= 0; j--) {
+            __Scratch_print_character_color(buf[j], attribute);
         }
     }
     update_cursor();
