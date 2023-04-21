ba78bb4212b1efa7f54f82cc478c1092234a5425
Anthony Panarello <aPanarello20@gmail.com>
2023-04-21 08:53:06 -0400

    Changed non-colored printing macros to use Scratch_get_active_attribute() instead of DEFAULT_ATTR to avoid coloring artifacts

--- include/Console.h
+++ include/Console.h
@@ -1,19 +1,21 @@
 #ifndef SCRATCH_CONSOLE_H_
 #define SCRATCH_CONSOLE_H_
 
+#define NULL ((void *)0)
+
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((char *)0xb8000)
 
-#define DEFAULT_ATTR 0x07
+#define print_character(c) print_character_color((char)c, Scratch_get_active_attribute())
+#define print_string(str) print_string_color((const char *)str, Scratch_get_active_attribute())
+#define print_line(str) print_line_color((const char *)str, Scratch_get_active_attribute())
+#define print_int(i) print_int_color((int)i, Scratch_get_active_attribute())
+#define print_uint(i) print_uint_color((unsigned int)i, Scratch_get_active_attribute())
+#define print_hex(a, b) print_hex_color((unsigned int)a, (int)b, Scratch_get_active_attribute())
 
-#define print_character(c) print_character_color((char)c, DEFAULT_ATTR)
-#define print_string(str) print_string_color((const char *)str, DEFAULT_ATTR)
-#define print_line(str) print_line_color((const char *)str, DEFAULT_ATTR)
-#define print_int(i) print_int_color((int)i, DEFAULT_ATTR)
-#define print_uint(i) print_uint_color((unsigned int)i, DEFAULT_ATTR)
-#define print_hex(a, b) print_hex_color((unsigned int)a, (int)b, DEFAULT_ATTR)
-
+unsigned char Scratch_get_active_attribute(void);
+void Scratch_set_active_attribute(unsigned char a);
 void clear_screen(void);
 void print_character_color(char character, unsigned char attribute);
 void print_string_color(const char * str, unsigned char attribute);
